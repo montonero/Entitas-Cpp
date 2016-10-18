5a34bedbf15e28e9d0f83f84fffbd7aafa259853
igor <igor@isqg.net>
2016-10-18 21:41:46 +0100

    sample added

---
+++ sample/sample1.cpp
@@ -0,0 +1,63 @@
+#include "EntitasPP/SystemContainer.hpp"
+#include "EntitasPP/Matcher.hpp"
+#include "EntitasPP/Pool.hpp"
+#include <iostream>
+
+using namespace EntitasPP;
+
+class DemoComponent : public IComponent {
+public:
+    void Reset(const std::string& name1, const std::string& name2) {
+        std::cout << "Created new entity: " << name1 << "," << name2 << std::endl;
+    }
+};
+
+class DemoSystem : public IInitializeSystem, public IExecuteSystem, public ISetPoolSystem {
+public:
+    void SetPool(Pool* pool) {
+        mPool = pool;
+    }
+    void Initialize() {
+        mPool->CreateEntity()->Add<DemoComponent>("foo", "bar");
+        std::cout << "DemoSystem initialized" << std::endl;
+    }
+    void Execute() {
+        mPool->CreateEntity()->Add<DemoComponent>("foo", "bar");
+
+        auto entitiesCount = mPool->GetGroup(Matcher_AllOf(DemoComponent))->Count();
+        std::cout << "There are " << entitiesCount << " entities with the component 'DemoComponent'" << std::endl;
+
+        std::cout << "DemoSystem executed" << std::endl;
+    }
+
+private:
+    Pool* mPool;
+};
+
+class Position : public IComponent {
+public:
+    // You must provide at least ONE public "Reset" method with any parameters you want
+    void Reset(float px, float py, float pz) {
+        x = px;
+        y = py;
+        z = pz;
+    }
+
+    float x;
+    float y;
+    float z;
+};
+
+int main(const int argc, const char* argv[]) {
+    auto systems = std::make_shared<SystemContainer>();
+    auto pool = std::make_shared<Pool>();
+
+    systems->Add(pool->CreateSystem<DemoSystem>());
+    systems->Initialize();
+
+    for(unsigned int i = 0; i < 2; ++i) {
+        systems->Execute();
+    }
+
+    return 0;
+}
---
+++ wscript
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+import sys
+import commands
+import os
+import os.path
+
+# the following two variables are used by the target "waf dist"
+VERSION='0.0.1'
+APPNAME='entitas'
+
+# these variables are mandatory ('/' are converted automatically)
+top = '.'
+out = 'build'
+
+def options(ctx):
+    ctx.load('compiler_c compiler_cxx')
+    ctx.add_option('--boost-path', dest='boost_path', help='Boost path.')
+    ctx.add_option('--wspp-path', dest='wspp_path', help='Websocket++ path.')
+
+def configure(ctx):
+    # ctx.env.ws_client_dir = os.path.abspath(os.path.join(ctx.path.parent.abspath(), 'client'))
+    # ctx.recurse(ctx.env.ws_client_dir)
+    ctx.load('compiler_c compiler_cxx')
+
+def build(ctx):
+    # libs = ['SDL2', 'chibi_static_lib', 'BOOST', 'ws_client_lib', 'ws_retry_client']
+    libs = []
+
+    # ctx.recurse('chibi')
+    # ctx.recurse(ctx.env.ws_client_dir)
+
+    s1 = ctx.path.ant_glob(['*.cpp', 'EntitasPP/*.cpp'])
+    ctx.program(
+        source=s1,
+        features='cxx cxxprogram',
+        target='app',
+        cxxflags     = ['-std=c++14'],
+        # linkflags = ['-Wl', '-lm', '-lpthread', '-lc', '-lstdc++'],
+        # linkflags = ['-Wl,-Bdynamic', '-lm', '-lpthread', '-lc', '-lstdc++'],
+        use = libs 
+    )
+
+    if ctx.cmd != 'clean':
+        from waflib import Logs
+        ctx.logger = Logs.make_logger('test.log', 'build') # just to get a clean output
+        ctx.logger = None
