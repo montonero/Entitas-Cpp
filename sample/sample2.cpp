98136d7377dfffa2f5ff851878d33957dea77604
Igor Makaruks <im@Igors-iMac.local>
2016-11-24 12:44:26 +0000

    osx samples

--- sample/sample2.cpp
+++ sample/sample2.cpp
@@ -4,7 +4,11 @@
 #include "entitas/ISystem.hpp"
 #include <iostream>
 
-#include "SDL.h"
+#ifdef __APPLE__
+    #include "SDL2/SDL.h"
+#else
+    #include "SDL.h"
+#endif
 
 #include "Rectangle.h"
 
